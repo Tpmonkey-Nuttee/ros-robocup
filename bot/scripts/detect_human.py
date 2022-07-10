c5d7f2fb0d2dde82e6ee284c1aee9c680713a43a
Tpmonkey <56111402+Tpmonkey-Nuttee@users.noreply.github.com>
2022-07-10 12:26:26 +0800

    Add files via upload

---
+++ bot/scripts/detect_human.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python
+import rospy
+from robot_vision_msgs.msg import BoundingBoxes
+from std_msgs.msg import String
+
+class Node:
+    def __init__(self):
+
+        self.enable = False
+
+        rospy.Subscriber("bot/start_human_dec", String, self.callenable)
+        rospy.Subscriber("yolo_ros/bounding_boxes", BoundingBoxes, self.callback)
+        self.pub = rospy.Publisher("bot/found_human", String, queue_size=10)
+
+    def callback(self, data):
+        if not self.enable:
+            return
+        else:
+            for bounding_box in data.bounding_boxes:
+                # print(data.bounding_boxes)
+                if bounding_box.Class == 'person':
+                    self.enable = False
+                    self.pub.publish("FoundPerson")
+                    break
+                # xo = (bounding_box.xmin+bounding_box.xmax)//2
+                # yo = (bounding_box.ymin+bounding_box.ymax)//2
+                # 	print xo,' ',yo 
+
+    def callenable(self,data):
+        self.enable = True
+
+
+if __name__ == '__main__':
+    rospy.init_node("detect_human")
+    node = Node()
+    rospy.spin()
